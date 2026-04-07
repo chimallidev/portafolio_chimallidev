b0cda495cb079e5ef741bec1ce2e2d4795f35ce9
chimallidev <chimalli.dev@gmail.com>
2026-04-06 19:11:17 -0600

    optimización de imagenes

--- app/core/constants.py
+++ app/core/constants.py
@@ -14,20 +14,20 @@ REDES = {
 #Habilidades
 SKILLS = {
     "principal" : [
-        {"title": "Python", "alt": "Logo de python","img": "/static/img/icons/chimallidev.ico"}
+        {"title": "Python", "alt": "Logo de python","img": "/static/img/python.webp"}
     ],
     "frontend" : [
-        {"title": "HTML", "alt": "Logo de html","img": "/static/img/icons/chimallidev.ico"},
-        {"title": "CSS", "alt": "Logo de css","img": "/static/img/icons/chimallidev.ico"},
-        {"title": "JS", "alt": "Logo de javascript","img": "/static/img/icons/chimallidev.ico"}
+        {"title": "HTML", "alt": "Logo de html","img": "/static/img/html.webp"},
+        {"title": "CSS", "alt": "Logo de css","img": "/static/img/css.webp"},
+        {"title": "JS", "alt": "Logo de javascript","img": "/static/img/js.webp"}
     ],
     "backend" : [
-        {"title": "FastAPI", "alt": "Logo de fastapi","img": "/static/img/icons/chimallidev.ico"},
-        {"title": "Jinja2", "alt": "Logo de jinja2","img": "/static/img/icons/chimallidev.ico"}
+        {"title": "FastAPI", "alt": "Logo de fastapi","img": "/static/img/fastapi.webp"},
+        {"title": "Jinja2", "alt": "Logo de jinja2","img": "/static/img/jinja.webp"}
     ],
     "herramientas" : [
-        {"title": "Docker", "alt": "Logo de docker","img": "/static/img/icons/chimallidev.ico"},
-        {"title": "Git", "alt": "Logo de Git","img": "/static/img/icons/chimallidev.ico"},
-        {"title": "GitHub", "alt": "Logo de github","img": "/static/img/icons/chimallidev.ico"}
+        {"title": "Docker", "alt": "Logo de docker","img": "/static/img/docker.webp"},
+        {"title": "Git", "alt": "Logo de Git","img": "/static/img/git.webp"},
+        {"title": "GitHub", "alt": "Logo de github","img": "/static/img/github.webp"}
     ]
 }
