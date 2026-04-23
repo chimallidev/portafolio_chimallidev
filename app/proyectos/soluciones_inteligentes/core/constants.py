f7c8212f448a64c8026d9a4c781f2df3bade3e25
chimallidev <chimalli.dev@gmail.com>
2026-04-23 14:44:52 -0600

    Videos del slider actualizados

--- app/proyectos/soluciones_inteligentes/core/constants.py
+++ app/proyectos/soluciones_inteligentes/core/constants.py
@@ -18,14 +18,22 @@ SLIDER_IMAGES = [
 ]
 
 VIDEOS_SLIDER = [
-    {"url": "https://img.youtube.com/vi/dx5On4Y54mA/hqdefault.jpg",
-     "id": "dx5On4Y54mA"},
-    {"url": "https://img.youtube.com/vi/dx5On4Y54mA/hqdefault.jpg",
-     "id": "aziWSKHBexg"},
-    {"url": "https://img.youtube.com/vi/dx5On4Y54mA/hqdefault.jpg",
-     "id": "dx5On4Y54mA"},
-    {"url": "https://img.youtube.com/vi/dx5On4Y54mA/hqdefault.jpg",
-     "id": "dx5On4Y54mA"},
-    {"url": "https://img.youtube.com/vi/dx5On4Y54mA/hqdefault.jpg",
-     "id": "dx5On4Y54mA"}
+    {"url": "https://img.youtube.com/vi/lxFpyyKrFbk/hqdefault.jpg",
+     "id": "lxFpyyKrFbk"},
+    {"url": "https://img.youtube.com/vi/c2iGyWguHtA/hqdefault.jpg",
+     "id": "c2iGyWguHtA"},
+    {"url": "https://img.youtube.com/vi/0s5kbjin1Fo/hqdefault.jpg",
+     "id": "0s5kbjin1Fo"},
+    {"url": "https://img.youtube.com/vi/UycGIvnYhFo/hqdefault.jpg",
+     "id": "UycGIvnYhFo"},
+     {"url": "https://img.youtube.com/vi/fAkD5kyNRn4/hqdefault.jpg",
+     "id": "fAkD5kyNRn4"},
+     {"url": "https://img.youtube.com/vi/-z5iNnUCDA0/hqdefault.jpg",
+     "id": "-z5iNnUCDA0"},
+     {"url": "https://img.youtube.com/vi/n-swnAW9eCo/hqdefault.jpg",
+     "id": "n-swnAW9eCo"},
+     {"url": "https://img.youtube.com/vi/lWmUQOsyQWk/hqdefault.jpg",
+     "id": "lWmUQOsyQWk"},
+    {"url": "https://img.youtube.com/vi/Pl5kW2G8qe0/hqdefault.jpg",
+     "id": "Pl5kW2G8qe0"}
 ]
