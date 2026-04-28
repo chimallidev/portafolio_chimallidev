bd96a59da2d9ba30e7a6a4a260cf548ce5c5f9dd
chimallidev <chimalli.dev@gmail.com>
2026-04-27 19:50:20 -0600

    Plantilla HTML de la base del proyecto

---
+++ app/proyectos/app_movil_soluciones_inteligentes/core/constants.py
@@ -0,0 +1,5 @@
+#Meta
+APP_MOVIL_SOLUCIONES_INTELIGENTES_META = {
+    "titulo": "App Móvil Soluciones Inteligentes",
+    "descripcion": "___"
+}
--- app/routes/proyectos/app_movil_soluciones_inteligentes/app_movil_soluciones_inteligentes.py
+++ app/routes/proyectos/app_movil_soluciones_inteligentes/app_movil_soluciones_inteligentes.py
@@ -1,13 +1,17 @@
 from fastapi import APIRouter, Request, HTTPException, status
 from fastapi.responses import HTMLResponse
 from app.core.templates import templates
+from app.proyectos.app_movil_soluciones_inteligentes.core.constants import APP_MOVIL_SOLUCIONES_INTELIGENTES_META
 
 router = APIRouter()
 
 @router.get("/portafolio/app-movil-soluciones-inteligentes", response_class= HTMLResponse, status_code= status.HTTP_200_OK)
 async def app_movil_soluciones_inteligentes(request: Request):
+    context = {
+        "meta_app_movil_soluciones_inteligentes" : APP_MOVIL_SOLUCIONES_INTELIGENTES_META
+    }
     try:
-        return templates.TemplateResponse(request= request, name= "/proyectos/app_movil_soluciones_inteligentes/app_movil_soluciones_inteligentes.html", context= {})
+        return templates.TemplateResponse(request= request, name= "/proyectos/app_movil_soluciones_inteligentes/app_movil_soluciones_inteligentes.html", context= context)
     except:
         raise HTTPException(status_code= status.HTTP_404_NOT_FOUND,
                             detail= "La página de la aplicación móvil de soluciones inteligentes no ha sido encontrada.")
