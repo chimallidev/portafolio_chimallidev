86e1a3159851d7c7cb7a1a80215e99189068c7d8
chimallidev <chimalli.dev@gmail.com>
2026-04-28 19:55:52 -0600

    Texto animado en el h1 del proyecto

--- app/proyectos/app_movil_soluciones_inteligentes/core/constants.py
+++ app/proyectos/app_movil_soluciones_inteligentes/core/constants.py
@@ -1,9 +1,5 @@
 #Meta
 APP_MOVIL_SOLUCIONES_INTELIGENTES_META = {
     "titulo": "App Móvil Soluciones Inteligentes",
-    "descripcion": """
-    Aplicación móvil de Soluciones Inteligentes desarrollada con Kotlin y 
-    Android Studio que centraliza contenido de fitness, alimentación y salud, 
-    con herramientas para mejorar el entrenamiento y hábitos saludables.
-    """
+    "descripcion": "Aplicación móvil de Soluciones Inteligentes desarrollada con Kotlin y Android Studio que centraliza contenido de fitness, alimentación y salud, con herramientas para mejorar el entrenamiento y hábitos saludables."
 }
