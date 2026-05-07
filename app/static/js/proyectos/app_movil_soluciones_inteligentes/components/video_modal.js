6b7a81db8da05fdb9ba969cb7c8b39357518e6ba
chimallidev <chimalli.dev@gmail.com>
2026-05-06 20:04:50 -0600

    Script del video modal

---
+++ app/static/js/proyectos/app_movil_soluciones_inteligentes/components/video_modal.js
@@ -0,0 +1,59 @@
+class VideoModal {
+    constructor(root){
+        this.root = root
+        this.modalVideo = root.querySelector("[data-modal-video]")
+
+        this.init()
+    }
+
+    init(){
+        this.bindEvents()
+    }
+
+    bindEvents(){
+        this.abrirModal()
+        this.cerrarModal()
+        this.escKey()
+    }
+
+    abrirModal(){
+        document.querySelectorAll(".video__thumb").forEach(thumb => {
+            thumb.addEventListener("click", (e)=>{
+                e.stopPropagation()
+
+                const videoId = thumb.dataset.videoId
+
+                this.root.classList.add("active")
+                this.modalVideo.src = `https://www.youtube.com/embed/${videoId}?autoplay=1`;
+
+            })
+        })
+    }
+
+    cerrarModal(){
+        this.root.addEventListener("click", (e) => {
+            if(e.target === this.root){
+                this.root.classList.remove("active")
+
+                this.modalVideo.src= ""
+            }
+        })
+    }
+
+    escKey(){
+        document.addEventListener("keydown", (e)=>{
+            if(e.key === "Escape"){
+                this.root.classList.remove("active")
+                this.modalVideo.src= ""
+            }
+        })
+    }
+}
+
+export function initVideoModals(){
+    const videoModals = document.querySelectorAll("[data-video-modal]")
+
+    videoModals.forEach(modal => {
+        new VideoModal(modal)
+    })
+}
