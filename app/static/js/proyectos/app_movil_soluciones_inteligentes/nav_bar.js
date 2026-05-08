f7661b968a76f22b7589109fa197cad0657f83ec
chimallidev <chimalli.dev@gmail.com>
2026-05-08 14:26:17 -0600

    Script de la barra de navegación

---
+++ app/static/js/proyectos/app_movil_soluciones_inteligentes/nav_bar.js
@@ -0,0 +1,28 @@
+let lastScroll = 0
+
+export function initNavbar(navigation){
+    const navbar = document.getElementById("navbar")
+
+    window.addEventListener('scroll', () => {
+
+        if(navigation.isNavbarLocked()) return
+        if(
+            document.body.classList.contains("menu-open") ||
+            navigation.isClickScrolling()
+        ) return
+
+        const currentScroll = window.scrollY
+
+        if(currentScroll > lastScroll){
+            navbar.classList.add("hidden-scroll")
+        } else {
+            navbar.classList.remove("hidden-scroll")
+        }
+
+        lastScroll = currentScroll
+    })
+}
+
+export function resetNavbarScroll(){
+    lastScroll = window.scrollY
+}
