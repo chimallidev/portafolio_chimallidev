b34e038a48b1bb2e4b45b0f6639332ba1573d6b9
chimallidev <chimalli.dev@gmail.com>
2026-08-11 17:32:24 -0600

    Error: problemas con las funciones exportadas en navigation

--- app/static/js/navbar.js
+++ app/static/js/navbar.js
@@ -1,27 +1,35 @@
-export function initNavbar(navigation){
+export function initNavbar(navigation) {
     const navbar = document.getElementById("navbar")
 
     let lastScroll = 0
 
-    window.addEventListener("scroll", ()=> {
+    window.addEventListener("scroll", () => {
         if (navigation.isNavbarLocked()) return
+
         if (
-            document.body.classList.contains("menu-open") || 
+            document.body.classList.contains("menu-open") ||
             navigation.isClickScrolling()
         ) return
 
         const currentScroll = window.scrollY
 
-        if(currentScroll > lastScroll){
+        if (currentScroll > lastScroll) {
             navbar.classList.add("hidden-scroll")
         } else {
             navbar.classList.remove("hidden-scroll")
         }
 
         lastScroll = currentScroll
-
-        if(navigation.isClickScrolling()) return
     })
+
+    window.addEventListener("wheel", () => {
+        navigation.unlockNavbar()
+    })
+
+    window.addEventListener("touchstart", () => {
+        navigation.unlockNavbar()
+    })
+
 }
 
 let lastScroll = 0
@@ -29,11 +37,3 @@ let lastScroll = 0
 export function resetNavbarScroll() {
     lastScroll = window.scrollY
 }
-
-window.addEventListener("wheel", () => {
-    navigation.unlockNavbar();
-})
-
-window.addEventListener("touchstart", () => {
-    navigation.unlockNavbar();
-})
